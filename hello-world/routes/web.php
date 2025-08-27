0f6085af01fef3c6e8f6208410a71eeb852746f8
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-08-27 19:52:39 -0300

    feat: Introdução ao sistema de roteamento

--- hello-world/routes/web.php
+++ hello-world/routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello-world', function () {
     return view('hello-world');
 });
+
